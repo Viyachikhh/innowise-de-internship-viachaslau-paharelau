314de7c26ee1bcb76f96a59c673b88a9288b5791
Viyachikhh <vyachik2k1@gmail.com>
2025-12-01 08:55:40 +0100

    airflow: completed task, waiting for edits

--- airflow_module/dags/etl_dag.py
+++ airflow_module/dags/etl_dag.py
@@ -11,13 +11,14 @@ from datetime import timedelta
 import time
 
 
-asset_df = Asset(uri="/home/viyachikhh/projects/innowise-internship/airflow_module/file_storage/preprocessed/data.csv", name='preprocessed_csv_path')
+asset_df = Asset(uri="/home/viyachikhh/projects/innowise-internship/airflow_module/file_storage/preprocessed/data.csv", name='preprocessed_csv')
 
 with DAG(dag_id='lms_dag_one', 
     schedule='@daily', 
     start_date=datetime(2025, 10, 10),
     tags=['lms', 'study'],
-    description="First dag which check file existence and preprocessing with pandas"):
+    description="First dag which check file existence and preprocessing with pandas",
+    dagrun_timeout=timedelta(minutes=3)):
     
     @task.sensor(task_id='waiting_for_file', poke_interval=120, timeout=600, mode='reschedule')
     def check_file_exists():
@@ -46,40 +47,40 @@ with DAG(dag_id='lms_dag_one',
 
     @task_group(group_id='preprocessing')
     def preprocessing():
-        
-        @task(task_id='df_loading')
-        def load_file(filename: str) -> pd.DataFrame:
-            df = pd.read_csv(filename)
-            return df[:50000]
 
-        @task(task_id='df_replacing_nan', execution_timeout=timedelta(minutes=2))
-        def replace_nan(df: pd.DataFrame) -> pd.DataFrame:
+        @task(task_id='df_replacing_nan')
+        def replace_nan(filename: str) -> str:
+            df = pd.read_csv(filename)
             df.fillna('-', inplace=True)
-            return df
+            df.to_csv(filename, index=False)
+            return filename
 
         @task(task_id='df_sorting')
-        def sort(df: pd.DataFrame) -> pd.DataFrame:
+        def sort(filename: str) -> str:
+            df = pd.read_csv(filename)
             df.sort_values(by=['at'], inplace=True)
-            return df
+            df.to_csv(filename, index=False)
+            return filename
 
-        @task(task_id='df_content_cleaning', outlets=[asset_df])
-        def clean(df: pd.DataFrame):
+        @task(task_id='df_content_cleaning')
+        def clean(filename: str):
+            df = pd.read_csv(filename)
             df['content'] = df['content'].str.replace(r'[^a-zA-Z0-9\.,:!?;\'\"-]+', ' ', regex=True).str.strip()
-            return df
+            df.to_csv(filename, index=False)
+            return filename
 
         @task(task_id='df_saving', outlets=[asset_df])
-        def save(df: pd.DataFrame, new_path: str):
-            print(new_path)
+        def save_new_path(old_path: str, new_path: str):
+            df = pd.read_csv(old_path)
             df.to_csv(new_path, index=False)
 
-        old_path = Variable.get("raw_data_path_file", deserialize_json=True)
+        raw_path = Variable.get("raw_data_path_file", deserialize_json=True)
         new_path = Variable.get("prep_data_path_file", deserialize_json=True)
 
-        df = load_file(old_path)
-        df_replaced = replace_nan(df)
-        df_sorted = sort(df_replaced)
-        df_cleaned = clean(df_sorted)
-        save(df_cleaned, new_path)
+        raw_path = replace_nan(raw_path)
+        raw_path = sort(raw_path)
+        raw_path = clean(raw_path)
+        save_new_path(raw_path, new_path)
 
     check_file_exists() >> define_flow_path() >> [bash_log(), preprocessing()]
 
@@ -89,18 +90,13 @@ with DAG(dag_id='lms_dag_two',
     schedule=[asset_df], 
     start_date=datetime(2025, 10, 10),
     tags=['lms', 'study'],
-    description="Second dag triggered by Asset from first dag"):
+    description="Second dag triggered by Asset from first dag",
+    dagrun_timeout=timedelta(minutes=2)):
 
-
-    @task(task_id='df_loading_preprocessed')
-    def load_file() -> pd.DataFrame:
-        path = Variable.get("prep_data_path_file", deserialize_json=True)
-        df = pd.read_csv(path)
-        return df
-    
     @task(task_id='df_inserting_to_database')
-    def load_to_database(df: pd.DataFrame):
-
+    def load_to_database(filename=Variable.get("prep_data_path_file", deserialize_json=True)):
+        
+        df = pd.read_csv(filename)
         db_name = Variable.get("db_study_name", deserialize_json=True)
 
         hook = MongoHook(mongo_conn_id="mongo_default")
@@ -112,6 +108,5 @@ with DAG(dag_id='lms_dag_two',
         collection.insert_many(data_dict)
 
 
-    df = load_file()
-    load_to_database(df)
+    load_to_database()
     # Path(path).unlink(missing_ok=True)
