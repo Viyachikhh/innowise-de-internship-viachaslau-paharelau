fa36a0a76d90a4fc2fd7abe1d51d4bcde272d740
Viyachikhh <vyachik2k1@gmail.com>
2025-12-17 11:46:48 +0100

    airflow: added Dockerfile and docker-compose, updated etl_dags with docker features

--- airflow_module/dags/etl_dag.py
+++ airflow_module/dags/etl_dag.py
@@ -1,6 +1,8 @@
+import os
 import pandas as pd
+import time
 
-from airflow.sdk import dag, DAG, Asset, task, Variable, Connection, task_group, get_current_context
+from airflow.sdk import dag, DAG, Asset, task, Variable, task_group
 from airflow.sdk.bases.sensor import PokeReturnValue
 from airflow.providers.mongo.hooks.mongo import MongoHook
 
@@ -8,10 +10,9 @@ from pathlib import Path
 from pendulum import datetime
 from datetime import timedelta
 
-import time
 
-
-asset_df = Asset(uri="/home/viyachikhh/projects/innowise-internship/airflow_module/file_storage/preprocessed/data.csv", name='preprocessed_csv')
+asset_df = Asset(uri=os.getenv('AIRFLOW_VAR_STORAGE') + '/preprocessed/data.csv', 
+                name='preprocessed_csv')
 
 with DAG(dag_id='lms_dag_one', 
     schedule='@daily', 
@@ -21,20 +22,26 @@ with DAG(dag_id='lms_dag_one',
     dagrun_timeout=timedelta(minutes=3)):
     
     @task.sensor(task_id='waiting_for_file', poke_interval=120, timeout=600, mode='reschedule')
-    def check_file_exists():
-        data_path = Path(Variable.get("raw_data_path_file", deserialize_json=True))
+    def check_file_exists(ti):
+        file_storage_path = Variable.get('STORAGE')
+        raw_path = file_storage_path + '/raw'
+        csv_list = list(Path(raw_path).glob("*.csv"))
     
-        if data_path.exists():
+        if len(csv_list) > 0:
+            xcom_value = str(csv_list[0])
             condition_met = True
+            ti.xcom_push(key='file_path', value=xcom_value)
         else:
+            xcom_value = None
             condition_met = False
 
-        return PokeReturnValue(is_done=condition_met, xcom_value=str(data_path))
+        return PokeReturnValue(is_done=condition_met, xcom_value=xcom_value)
 
 
     @task.branch(task_id='branching')
-    def define_flow_path():
-        if Path(Variable.get("raw_data_path_file", deserialize_json=True)).stat().st_size == 0:
+    def define_flow_path(ti):
+        csv_name = ti.xcom_pull(key="file_path", task_ids="waiting_for_file")
+        if pd.read_csv(csv_name).empty:
             return 'bash_script'
         else:
             return 'preprocessing'
@@ -74,8 +81,13 @@ with DAG(dag_id='lms_dag_one',
             df = pd.read_csv(old_path)
             df.to_csv(new_path, index=False)
 
-        raw_path = Variable.get("raw_data_path_file", deserialize_json=True)
-        new_path = Variable.get("prep_data_path_file", deserialize_json=True)
+        file_storage_path = Variable.get('storage')
+        raw_storage_path = file_storage_path + '/raw'
+        new_storage_path = file_storage_path + '/preprocessed'
+        csv_name = list(Path(raw_storage_path).glob("*.csv"))[0]
+
+        raw_path = str(csv_name)
+        new_path = new_storage_path + '/data.csv'
 
         raw_path = replace_nan(raw_path)
         raw_path = sort(raw_path)
@@ -94,19 +106,25 @@ with DAG(dag_id='lms_dag_two',
     dagrun_timeout=timedelta(minutes=2)):
 
     @task(task_id='df_inserting_to_database')
-    def load_to_database(filename=Variable.get("prep_data_path_file", deserialize_json=True)):
+    def load_to_database(filename: str):
         
         df = pd.read_csv(filename)
-        db_name = Variable.get("db_study_name", deserialize_json=True)
+        
+        db_name = Variable.get("db_name")
 
-        hook = MongoHook(mongo_conn_id="mongo_default")
+        hook = MongoHook(mongo_conn_id="mongo_conn")
+        
         db = hook.get_conn()[db_name]
-        collection = db[Variable.get("db_mongo_collection", deserialize_json=True)]
-
+      
+        collection = db[Variable.get("db_collection")]
+       
         df.reset_index(inplace=True)
         data_dict = df.to_dict("records")
+        
         collection.insert_many(data_dict)
+   
 
 
-    load_to_database()
+    filepath = Variable.get('storage') + '/preprocessed' + '/data.csv'
+    load_to_database(filepath)
     # Path(path).unlink(missing_ok=True)
