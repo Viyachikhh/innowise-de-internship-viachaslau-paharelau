cfe9b9ae46826e5e06aa1aa8664547b253d78be5
Viyachikhh <vyachik2k1@gmail.com>
2025-12-28 10:16:25 +0100

    bi: updated data_prep example .py file, updated docker compose, deleted python app

--- bi_module/data_preprocessing.py
+++ bi_module/data_preprocessing.py
@@ -1,12 +1,29 @@
-import sys
-import os
+import pandas as pd
+import numpy as np
 
-print(f"Привет из Docker! Версия Python: {sys.version}")
-print("Текущая директория:", os.getcwd())
 
-# Проверка установки библиотек (если прописали в requirements.txt)
-try:
-    import pandas as pd
-    print("Библиотека Pandas успешно установлена!")
-except ImportError:
-    print("Pandas не установлен.")
+def scd1():
+    return new_names[np.random.randint(len(new_names))]
+
+
+def scd2():
+    return new_locations[np.random.randint(len(new_locations))]
+
+df = pd.read_csv("file_storage/original_data.csv")
+
+new_names = ["A", "B", "C", "D", "E", "F", "G", "H"]
+new_locations = ["North-West", "North-East", "South-West", "South-East", "Center"]
+
+df_sample_copies = df.sample(200).drop_duplicates()
+print(df_sample_copies.shape)
+df_sample_dcp1 = df.sample(200).drop_duplicates()
+df_sample_dcp1["Customer Name"] = df_sample_dcp1["Customer Name"].apply(lambda x: scd1())
+# когда в одном файле есть несколько изменений имени, позже правильно обработаю
+df_sample_dcp1 = df_sample_dcp1.drop_duplicates(subset=["Customer ID"])                                                                                                             
+print(df_sample_dcp1.shape)
+df_sample_dcp2 = df.sample(200).drop_duplicates()
+df_sample_dcp2["Region"] = df_sample_dcp2["Region"].apply(lambda x: scd2())
+print(df_sample_dcp2.shape)
+
+df_concatenated = pd.concat([df_sample_copies,df_sample_dcp1, df_sample_dcp2]).drop_duplicates(subset=["Row ID"])
+df_concatenated.to_csv("csv_files/secondary_data.csv", index=False)
