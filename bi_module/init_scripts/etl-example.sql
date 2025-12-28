11cb3d87b8ba459683456f805d0c9bcfaa068562
Viyachikhh <vyachik2k1@gmail.com>
2025-12-28 10:15:32 +0100

    bi:updated sql scripts

--- bi_module/init_scripts/etl-example.sql
+++ bi_module/init_scripts/etl-example.sql
@@ -14,23 +14,6 @@ WITH (
 
 CALL stage.from_stage_to_core_v2();
 
-/*
-
-COPY stage.super_store (row_id, order_uniq_card, order_date, ship_date, 
-                        ship_mode, customer_uniq_card, customer_name,
-                        segment, country, city, _state, postal_code, 
-                        region, product_uniq_card, category, sub_category, 
-                        product_name, sales, quantity, discount, profit)
-FROM '/file_storage/dcp1.csv'
-WITH (
-    FORMAT csv,      -- указываем формат
-    HEADER true,     -- пропускаем первую строку (заголовок)
-    DELIMITER ','    -- разделитель (обычно запятая)
-);
-
-CALL stage.from_stage_to_core_v2();
-
-
 
 
 COPY stage.super_store (row_id, order_uniq_card, order_date, ship_date, 
@@ -38,15 +21,15 @@ COPY stage.super_store (row_id, order_uniq_card, order_date, ship_date,
                         segment, country, city, _state, postal_code, 
                         region, product_uniq_card, category, sub_category, 
                         product_name, sales, quantity, discount, profit)
-FROM '/file_storage/dcp2.csv'
+FROM '/file_storage/secondary_data.csv'
 WITH (
-    FORMAT csv,      -- указываем формат
-    HEADER true,     -- пропускаем первую строку (заголовок)
-    DELIMITER ','    -- разделитель (обычно запятая)
+    FORMAT csv,      
+    HEADER true,     
+    DELIMITER ',' 
 );
 
 CALL stage.from_stage_to_core_v2();
-*/
+
 
 
 ----------- в mart слой
