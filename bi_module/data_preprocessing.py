467d396b52a00cdd8cd40de3d3a0347ba107e853
Viyachikhh <vyachik2k1@gmail.com>
2025-12-30 11:05:33 +0100

    bi: returned python file for creating dynamic secondary template, all files was updated due new conditions

--- bi_module/data_preprocessing.py
+++ bi_module/data_preprocessing.py
@@ -14,20 +14,20 @@ df = pd.read_csv("file_storage/original_data.csv")
 new_names = ["A", "B", "C", "D", "E", "F", "G", "H"]
 new_locations = ["North-West", "North-East", "South-West", "South-East", "North"]
 
-df_sample_copies = df.sample(200).drop_duplicates(subset=["Row ID"])
+df_sample_copies = df[:500].sample(500).drop_duplicates(subset=["Row ID"])
 
-df_sample_dcp1 = df.sample(200).drop_duplicates(subset=["Row ID"])
+df_sample_dcp1 = df[375:875].sample(500).drop_duplicates(subset=["Row ID"])
 df_sample_dcp1["Customer Name"] = df_sample_dcp1["Customer Name"].apply(lambda x: scd1())
 df_sample_dcp1 = df_sample_dcp1.drop_duplicates()                                                                                                             
 
-df_sample_dcp2 = df.sample(200).drop_duplicates(subset=["Row ID"])
+df_sample_dcp2 = df[650:1150].sample(500).drop_duplicates(subset=["Row ID"])
 df_sample_dcp2["Region"] = df_sample_dcp2["Region"].apply(lambda x: scd2())
 df_sample_dcp2 = df_sample_dcp2.drop_duplicates()
 
-df_sample_combinative = df.sample(200).drop_duplicates(subset=["Row ID"])
+df_sample_combinative = df[1025:1625].sample(500).drop_duplicates(subset=["Row ID"])
 df_sample_combinative["Customer Name"] = df_sample_combinative["Customer Name"].apply(lambda x: scd1())
 df_sample_combinative["Region"] = df_sample_combinative["Region"].apply(lambda x: scd2())
 df_sample_combinative = df_sample_combinative.drop_duplicates()
 
 df_concatenated = pd.concat([df_sample_copies,df_sample_dcp1, df_sample_dcp2, df_sample_combinative]).drop_duplicates(subset=["Row ID"])
-df_concatenated.to_csv("csv_files/secondary_data.csv", index=False)
+df_concatenated.to_csv("file_storage/secondary_data.csv", index=False)
