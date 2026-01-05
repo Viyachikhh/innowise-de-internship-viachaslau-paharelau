7ed8d3dbc158358a3f7ae40bc2a2bca25e83286e
Viyachikhh <vyachik2k1@gmail.com>
2026-01-05 09:17:35 +0100

    alert: added entry main.py

---
+++ project_alert/main.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from src.df_preparation import reading, preparation
+from src.rules import BaseErrorCheck, BaseBundleErrorCheck
+from src.log_aggregator import LogAggregator
+
+
+def main():
+    folder_elements = list(Path("logs").rglob("*.csv"))
+    folder_elements = [str(path) for path in folder_elements]
+
+    names = [path.split('/')[-1] for path in folder_elements]
+    dfs = [preparation(reading(path)) for path in folder_elements]
+    dicts = dict(zip(names, dfs))
+    # print(dicts)
+    aggregator = LogAggregator(BaseErrorCheck, BaseBundleErrorCheck)
+    # print(aggregator)
+    error_list = aggregator.extract_fields(**dicts)
+
+    print(error_list[0])
+
+
+
+
+if __name__ == "__main__":
+    main()
